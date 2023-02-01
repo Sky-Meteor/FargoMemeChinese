77852e582f3d20d889ac1759b0647cfffca696b4
Sky-Meteor <2746501855@qq.com>
2023-02-01 13:18:52 +0800

    qzkn & esc

---
+++ FMCGlobalItem.cs
@@ -0,0 +1,19 @@
+﻿using FargoMemeChinese.UnmanagedTranslations;
+using FargowiltasSouls.Items.Summons;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace FargoMemeChinese
+{
+    public class FMCGlobalItem : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.type == ModContent.ItemType<MutantsCurse>() && Main.LocalPlayer.name.Contains("清州可奈"))
+            {
+                tooltips.Find(l => l.Mod == "Terraria" && l.Name == "ItemName").Text = "清州可奈快乐器";
+            }
+        }
+    }
+}
